f308c62c0356cde24f1117ee13fc68466d7965a5
pujie <puji@gmail.com>
2018-09-14 11:06:28 +0700

    add queries for offers

--- index.js
+++ index.js
@@ -11,17 +11,76 @@ app.use((req,res,next)=>{
     
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-app.post("/save",(req,res) => {
-    console.log("req.params",req.body)
-    db.execute(sql.save(req.body), result => {
+app.post("/visitsave",(req,res) => {
+    console.log("req.body",req.body)
+    db.execute(sql.visitsave(req.body), result => {
+        console.log("visitsave result",result)
+        res.send(result)
+    })
+})
+app.get("/visitgets",(req,res) => {
+    console.log("visitgets",req.params)
+    db.execute(sql.visitgets(), result => {
+        res.send(result)
+    })
+})
+app.get("/visitget/:id",(req,res) => {
+    console.log("visitget",req.params)
+    db.execute(sql.visitget(req.params), result => {
+        res.send(result)
+    })
+})
+app.post("/visitupdate",(req,res) => {
+    console.log("req.body",req.body)
+    db.execute(sql.visitupdate(req.body), result => {
+        console.log("visitupdate result",result)
+        res.send(result)
+    })
+})
+app.get("/visitremove/:id",(req,res) => {
+    console.log("visitremove",req.params)
+    db.execute(sql.visitremove(req.params), result => {
+        res.send(result)
+    })
+})
+
+
+
+
+
+
+
+app.post("/offersave",(req,res) => {
+    console.log("req.body",req.body)
+    db.execute(sql.offersave(req.body), result => {
         console.log("save result",result)
         res.send(result)
     })
 })
-app.get("/get",(req,res) => {
-    console.log("get",req.params)
-    db.execute(sql.get(), result => {
+app.get("/offergets",(req,res) => {
+    console.log("offergets",req.params)
+    db.execute(sql.offergets(), result => {
         res.send(result)
     })
 })
+app.get("/offerget/:id",(req,res) => {
+    console.log("offerget",req.params)
+    db.execute(sql.offerget(req.params), result => {
+        res.send(result)
+    })
+})
+app.post("/offerupdate",(req,res) => {
+    console.log("req.body",req.body)
+    db.execute(sql.offerupdate(req.body), result => {
+        console.log("offerupdate result",result)
+        res.send(result)
+    })
+})
+app.get("/offerremove/:id",(req,res) => {
+    console.log("offerremove",req.params)
+    db.execute(sql.offerremove(req.params), result => {
+        res.send(result)
+    })
+})
+
 app.listen(process.env.PORT || 2019)
--- js/queries.js
+++ js/queries.js
@@ -1,18 +1,90 @@
 var 
-save = obj => {
+visitsave = obj => {
     sql = 'insert into visitentries '
-    sql+= '(client,address,pic,position,phone,aim ) '
+    sql+= '(client,address,pic,position,phone,aim,transport ) '
     sql+= 'values '
-    sql+= '("'+obj.client+'","'+obj.address+'","'+obj.pic+'","'+obj.position+'","'+obj.phone+'","'+obj.aim+'") '
+    sql+= '("'+obj.client+'","'+obj.address+'","'+obj.pic+'","'+obj.position+'","'+obj.phone+'","'+obj.aim+'","'+obj.transport+'") '
     console.log("Save query",sql)
     return sql
 },
-get = () => {
+visitgets = () => {
     sql = 'select * from visitentries '
+    console.log("gets query",sql)
+    return sql
+},
+visitget = obj => {
+    sql = 'select * from visitentries '
+    sql+= 'where id = ' + obj.id
     console.log("get query",sql)
     return sql
+},
+visitupdate = obj => {
+    sql = 'update visitentries '
+    sql+= 'set client="'+obj.client+'",'
+    sql+= 'address="'+obj.address+'",'
+    sql+= 'pic="'+obj.pic+'",'
+    sql+= 'position="'+obj.position+'",'
+    sql+= 'phone="'+obj.phone+'",'
+    sql+= 'aim="'+obj.aim+'", '
+    sql+= 'transport="'+obj.transport+'" '
+    sql+= 'where id = ' + obj.id
+    console.log("Update query",sql)
+    return sql
+},
+visitremove = obj => {
+    sql = 'delete from visitentries '
+    sql+= 'where id = ' + obj.id + ' '
+    console.log('Remove query',sql)
+    return sql
+},
+offersave = obj => {
+    sql = 'insert into visitentries '
+    sql+= '(client,address,pic,position,phone,aim,transport ) '
+    sql+= 'values '
+    sql+= '("'+obj.client+'","'+obj.address+'","'+obj.pic+'","'+obj.position+'","'+obj.phone+'","'+obj.aim+'","'+obj.transport+'") '
+    console.log("Save query",sql)
+    return sql
+},
+offergets = () => {
+    sql = 'select * from visitentries '
+    console.log("gets query",sql)
+    return sql
+},
+offerget = obj => {
+    sql = 'select * from visitentries '
+    sql+= 'where id = ' + obj.id
+    console.log("get query",sql)
+    return sql
+},
+offerupdate = obj => {
+    sql = 'update visitentries '
+    sql+= 'set client="'+obj.client+'",'
+    sql+= 'address="'+obj.address+'",'
+    sql+= 'pic="'+obj.pic+'",'
+    sql+= 'position="'+obj.position+'",'
+    sql+= 'phone="'+obj.phone+'",'
+    sql+= 'aim="'+obj.aim+'", '
+    sql+= 'transport="'+obj.transport+'" '
+    sql+= 'where id = ' + obj.id
+    console.log("Update query",sql)
+    return sql
+},
+offerremove = obj => {
+    sql = 'delete from visitentries '
+    sql+= 'where id = ' + obj.id + ' '
+    console.log('Remove query',sql)
+    return sql
 }
+
 module.exports = {
-    get : get,
-    save : save
+    visitgets : visitgets,
+    visitget : visitget,
+    visitsave : visitsave,
+    visitupdate : visitupdate,
+    visitremove : visitremove,
+    offergets : offergets,
+    offerget : offerget,
+    offersave : offersave,
+    offerupdate : offerupdate,
+    offerremove : offerremove
 }
