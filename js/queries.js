e6f76a9b8d43e6a13226e63c811c7c0d1c93bd61
pujie <puji@gmail.com>
2018-09-18 11:00:54 +0700

    fix offer query

--- js/queries.js
+++ js/queries.js
@@ -38,41 +38,60 @@ visitremove = obj => {
     return sql
 },
 offersave = obj => {
-    sql = 'insert into visitentries '
-    sql+= '(client,address,pic,position,phone,aim,transport ) '
+    sql = 'insert into offers '
+    sql+= '(kdoffer,offerdate,client_id,client_site_id,clientname,address,email,pic,phone,service,price,description,sale_id ) '
     sql+= 'values '
-    sql+= '("'+obj.client+'","'+obj.address+'","'+obj.pic+'","'+obj.position+'","'+obj.phone+'","'+obj.aim+'","'+obj.transport+'") '
-    console.log("Save query",sql)
+    sql+= '('
+    sql+= '"'+obj.kdoffer+'",'
+    sql+= '"'+obj.offerdate+'",'
+    sql+= '"'+obj.client_id+'",'
+    sql+= '"'+obj.client_site_id+'",'
+    sql+= '"'+obj.clientname+'",'
+    sql+= '"'+obj.address+'",'
+    sql+= '"'+obj.email+'",'
+    sql+= '"'+obj.pic+'",'
+    sql+= '"'+obj.phone+'",'
+    sql+= '"'+obj.service+'",'
+    sql+= '"'+obj.price+'",'
+    sql+= '"'+obj.description+'",'
+    sql+= '"'+obj.sale_id+'"'
+    sql+= ') '
+    console.log("Save Offer query",sql)
     return sql
 },
 offergets = () => {
-    sql = 'select * from visitentries '
-    console.log("gets query",sql)
+    sql = 'select * from offers '
+    console.log("gets Offer query",sql)
     return sql
 },
 offerget = obj => {
-    sql = 'select * from visitentries '
+    sql = 'select * from offers '
     sql+= 'where id = ' + obj.id
-    console.log("get query",sql)
+    console.log("get Offer query",sql)
     return sql
 },
 offerupdate = obj => {
-    sql = 'update visitentries '
-    sql+= 'set client="'+obj.client+'",'
+    sql = 'update offers '
+    sql+= 'set kdoffer="'+obj.kdoffer+'",'
+    sql+= 'client_id="'+obj.client_id+'",'
+    sql+= 'client_site_id="'+obj.client_site_id+'",'
+    sql+= 'clientname="'+obj.clientname+'",'
     sql+= 'address="'+obj.address+'",'
-    sql+= 'pic="'+obj.pic+'",'
-    sql+= 'position="'+obj.position+'",'
-    sql+= 'phone="'+obj.phone+'",'
-    sql+= 'aim="'+obj.aim+'", '
-    sql+= 'transport="'+obj.transport+'" '
+    sql+= 'email="'+obj.email+'", '
+    sql+= 'pic="'+obj.pic+'", '
+    sql+= 'phone="'+obj.phone+'", '
+    sql+= 'service="'+obj.service+'", '
+    sql+= 'price="'+obj.price+'", '
+    sql+= 'description="'+obj.description+'", '
+    sql+= 'sale_id="'+obj.sale_id+'" '
     sql+= 'where id = ' + obj.id
-    console.log("Update query",sql)
+    console.log("Update Offer query",sql)
     return sql
 },
 offerremove = obj => {
-    sql = 'delete from visitentries '
+    sql = 'delete from offers '
     sql+= 'where id = ' + obj.id + ' '
-    console.log('Remove query',sql)
+    console.log('Remove Offer query',sql)
     return sql
 }
 
